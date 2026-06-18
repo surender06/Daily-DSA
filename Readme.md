514f853826feeed485cfb173ea348d8ef5ec8ca6
surender06 <www.2surendar@gmail.com>
2026-06-18 13:55:44 +0530

    I solved the Array to Reversed Array

---
+++ Readme.md
@@ -0,0 +1,4 @@
+# DAY - 1
+reverse the array A = [1,2,3] -> [3,2,1]
+
+# Day -2
---
+++ day-1.py
@@ -0,0 +1,11 @@
+def reversearray(numbers):
+    numbers.reverse()
+    return numbers
+numbers = [1, 2, 3, 4, 5]
+ReversedArray=reversearray(numbers)
+print(ReversedArray)
+
+
+
+
+
