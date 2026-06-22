967de5a194dcdcf914031f0115e35b4c5ec8cecc
surender06 <www.2surendar@gmail.com>
2026-06-22 13:22:26 +0530

    Recovered day-1 day-2 and README

---
+++ day-2.py
@@ -0,0 +1,14 @@
+ODD_ARRAY=[]
+EVEN_ARRAY=[]
+def SplitArray(A):
+    for i in A:
+      if i%2==0:
+        odd=ODD_ARRAY.append(i)
+        
+      else:
+        even=EVEN_ARRAY.append(i)
+        
+A=[0,1,2,3,4,5,6,7,8,9] 
+SplitArray(A)    
+print("odd",ODD_ARRAY)
+print("even",EVEN_ARRAY)    
